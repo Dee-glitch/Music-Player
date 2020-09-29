ac8085eb2d434cb5f58ee8a92a780b548ddcfd6a
Deedee <you@example.com>
2020-09-29 12:59:41 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,151 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const volumeBtn = document.getElementById('btn--volume');
+const volumeControl = document.getElementById('volume-control');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+//Music
+const songs = [
+    {
+        name: 'MbiloMbilo-1',
+        displayName: 'Mbilo Mbilo',
+        artist:'Eddy Kenzo',
+    },
+    {
+        name: 'Mr.DJ-2',
+        displayName: 'Mr DJ',
+        artist:'Swangz Ave',
+    },
+    {
+        name: 'SityaLoss-3',
+        displayName: 'Sitya Loss',
+        artist:'Eddy Kenzo',
+    },
+    {
+        name: 'Tetema-4',
+        displayName: 'Tetema',
+        artist:'Rayvanny Ft Diamond Platnumz',
+    },
+    {
+        name: 'ValuValuva-5',
+        displayName: 'Valu Valu',
+        artist:'Chameleon',
+    },
+    {
+        name: 'Zindola-6',
+        displayName: 'Zindola',
+        artist:'Da Twinz Ft Alikibaindola',
+    },
+]
+
+//Check if Playing
+let isPlaying = false;
+
+// Play
+function playSong() {
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+  }  
+
+// Pause
+function pauseSong() {
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+  }  
+
+  //Volume
+  function setVolume(value) {
+    audio.volume = value;
+  }
+
+// Play or Pause Event Listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+//Update the DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+//Current Song
+let songIndex = 0;
+
+//Previous Song
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length -1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+//Next Song
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+//On Load - Select First Song
+loadSong(songs[songIndex]);
+
+//Update Progress Bar and Time
+function updateProgressBar(e) {
+    if (isPlaying) {
+        const {duration, currentTime } = e.srcElement;
+        //Update progress bar Width
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+        //Calculate display for Duration
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if (durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        //Delay Swutching duration Element to avoid NaN
+        if (durationSeconds) {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+        //Calculate display for Current
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if (currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+    }
+}
+
+//Set Progress Bar
+function setProgressBar(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const {duration} = music;
+    music.currentTime = (clickX / width) * duration;
+}
+
+// Event Listeners
+nextBtn.addEventListener('click', nextSong);
+prevBtn.addEventListener('click', prevSong);
+music.addEventListener('ended', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
